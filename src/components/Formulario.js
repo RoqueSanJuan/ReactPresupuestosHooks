65efe70dbf2e81ccb9e5265854c11bf5968f5e15
roque <roque.m.sanjuan@gmail.com>
2020-11-23 11:24:10 -0300

    App de Gastos Terminada

---
+++ src/components/Formulario.js
@@ -0,0 +1,79 @@
+import React , {useState} from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+
+const Formulario = ({setGasto, addCrearGasto}) => {
+
+
+    const [nombreGasto, setNombreGasto] = useState("");
+    const [cantidadGasto, setCantidadGasto] = useState(0);
+    const [error, setError] = useState(false);
+    
+    const addGasto = e => {
+        e.preventDefault();
+
+        if(cantidadGasto < 1 || isNaN(cantidadGasto ) || nombreGasto.trim === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+        
+        const Gasto = {
+            nombre : nombreGasto,
+            cantidad : cantidadGasto,
+            id : shortid.generate()
+        }
+
+        setGasto(Gasto);
+        addCrearGasto(true);
+
+        setNombreGasto("");
+        setCantidadGasto(0);
+        
+    }
+
+    return(
+
+        <form
+
+            onSubmit={addGasto}
+        >
+            <h2>Agrega tus Gastos</h2>
+
+            { error ? <Error mensaje="Presupuesto incorrecto"/> : null}
+
+            <div className="campo">
+                <label>Nombre Gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                    value={nombreGasto}
+                    onChange = {e => setNombreGasto(e.target.value)}
+                ></input>
+            </div>
+            <div className="campo">
+                <label>Cantidad Gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"
+                    value = {cantidadGasto}
+                    onChange = {e => setCantidadGasto(parseInt(e.target.value,10))}
+                ></input>
+            </div>
+            <input
+                type="submit"
+                className="button-primary u-full-wight"
+                value="Agregar Gasto"
+            ></input>
+        </form>
+
+    )
+
+}
+
+
+
+
+export default Formulario;
---
+++ src/components/Pregunta.js
@@ -0,0 +1,67 @@
+import React, { Fragment , useState} from 'react';
+import Error from './Error';
+
+
+const Pregunta = ({setPresupuesto, setRestante, setmostrarPregunta}) => {
+    
+    //Defino States
+    const [ presupuestoInicial, setPresupuestoInicial ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+
+    //Funcion que lee el presupusto
+    const definirPresupuesto = e => setPresupuestoInicial(parseInt(e.target.value),10);
+
+    //submit para definir el presupuesto
+
+    const asignoPresupuesto = e => {
+
+        //Evito la carga de la pagina
+        e.preventDefault();
+        
+        //Validar Presupuesto
+        if(presupuestoInicial < 1 || isNaN(presupuestoInicial)){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        setPresupuesto(presupuestoInicial);
+        setRestante(presupuestoInicial);
+        setmostrarPregunta(false);
+
+    }
+
+    return (
+        <Fragment>
+            <h2>
+                Coloca tu presupuesto
+            </h2>
+
+            {error ? <Error mensaje="El presupuesto es incorrecto " /> : null }
+
+            <form
+                onSubmit={asignoPresupuesto}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ingresa tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir presupuesto"
+                />
+            </form>
+
+        </Fragment>
+
+
+
+    )
+}
+
+
+export default Pregunta;
